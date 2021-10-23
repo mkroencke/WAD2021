93fc88876841e5b17c6e69bed742d630dc82d4d1
mkroencke <m.kroencke@outlook.de>
2021-10-23 18:33:52 +0300

    Added jQuery script for calling of API and for generating postings. Added json to repository.

---
+++ res/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+  $.get("https://api.jsonbin.io/b/617402ea9548541c29c76269/18", function(posts) {
+    let section = $("#postingSection");
+    for (post of posts) {
+      let article = $('<article>');
+      let header = $('<header class="post-info">');
+      let userpic = $('<img width="40" height="40">').attr("src","res/images/userpic.png").attr("alt","User")
+      let datetime = $('<time>').text(post.createdAt.date + ' ' + post.createdAt.time);
+      let p = $('<p>').text(post.content.text)
+      let div = $('<div class="like-button">')
+      let button = $('<button>')
+      let buttonImg = $('<img width="30" height="28">').attr("src","res/images/like.png").attr("alt","Like this post");
+
+      section.append(article)
+      article.append(header)
+      header.append(userpic)
+      header.append(datetime)
+      if(post.content.pic) {
+        let img = $('<img>').attr("src",post.content.pic);
+        article.append(img)
+      }
+      article.append(p)
+      article.append(div)
+      div.append(button)
+      button.append(buttonImg)
+    }
+  });
+
+});
