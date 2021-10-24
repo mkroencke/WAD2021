2ff8230357fa2f7b70984119c5af5a7eece0a3d3
mkroencke <m.kroencke@outlook.de>
2021-10-24 22:09:39 +0300

    Added and changed some postings; Added author to posting header; Added API Access link to README; Minor code formatting and refactoring

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # WAD2021
 Web Application Development 2021/22
 
+Group Members:
 Nikita Naumov, Andi Tali, Matthias Kröncke
+
+API Access URL:
+https://api.jsonbin.io/b/617402ea9548541c29c76269/22
+
+JSON Validator:
+https://jsonbin.io/json-validator
--- res/js/script.js
+++ res/js/script.js
@@ -1,42 +1,70 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  $.get(
+    "https://api.jsonbin.io/b/617402ea9548541c29c76269/22",
+    function (posts) {
+      let section = $("#postingSection");
+      for (post of posts) {
+        let article = $("<article>");
+        let header = $("<header>");
+        header.addClass("post-info");
 
-  $.get("https://api.jsonbin.io/b/617402ea9548541c29c76269/18", function(posts) {
-    let section = $("#postingSection");
-    for (post of posts) {
-      let article = $('<article>');
-      let header = $('<header class="post-info">');
-      let userpic = $('<img width="40" height="40">').attr("src","res/images/userpic.png").attr("alt","User")
-      let datetime = $('<time>').text(post.createdAt.date + ' ' + post.createdAt.time);
-      let p = $('<p>').text(post.content.text)
-      let div = $('<div class="like-button">')
-      let button = $('<button>')
-      let buttonImg = $('<img width="30" height="28">').attr("src","res/images/like.png").attr("alt","Like this post");
+        let divUser = $("<div>");
+        divUser.addClass("header-user");
+        let userpic = $("<img>").attr({
+          height: "40",
+          width: "40",
+          src: "res/images/userpic.png",
+          alt: "User",
+        });
+        let author = $("<p>");
+        author.text(post.author.firstname + " " + post.author.lastname);
+        author.addClass("author");
+        let datetime = $("<time>");
+        datetime = $("<time>").text(
+          post.createdAt.date + " " + post.createdAt.time
+        );
 
-      section.append(article)
-      article.append(header)
-      header.append(userpic)
-      header.append(datetime)
-      if(post.content.pic) {
-        let img = $('<img>').attr("src",post.content.pic);
-        article.append(img)
+        let postingText = $("<p>");
+        postingText.text(post.content.text);
+
+        let divButton = $("<div>");
+        divButton.addClass("like-button");
+        let button = $("<button>");
+        let buttonImg = $("<img>").attr({
+          height: "28",
+          width: "30",
+          src: "res/images/like.png",
+          alt: "Like this post",
+        });
+
+        section.append(article);
+        article.append(header);
+        header.append(divUser);
+        divUser.append(userpic);
+        divUser.append(author);
+        header.append(datetime);
+        if (post.content.pic) {
+          let img = $("<img>").attr("src", post.content.pic);
+          article.append(img);
+        }
+        article.append(postingText);
+        article.append(divButton);
+        divButton.append(button);
+        button.append(buttonImg);
       }
-      article.append(p)
-      article.append(div)
-      div.append(button)
-      button.append(buttonImg)
     }
-  });
+  );
 
-  $("#userpic").click(function() {
+  $("#userpic").click(function () {
     if ($("#namemail").length) {
-      $("#namemail").toggle()
+      $("#namemail").toggle();
     } else {
-      let namemail = $('<div id="namemail">')
-      let pname = $('<p>').text("John Doe")
-      let pemail = $('<p>').text("john.doe@example.com")
-      namemail.append(pname)
-      namemail.append(pemail)
-      $("#pageHeader").append(namemail)
+      let namemail = $('<div id="namemail">');
+      let pname = $("<p>").text("John Doe");
+      let pemail = $("<p>").text("john.doe@example.com");
+      namemail.append(pname);
+      namemail.append(pemail);
+      $("#pageHeader").append(namemail);
     }
   });
 });
